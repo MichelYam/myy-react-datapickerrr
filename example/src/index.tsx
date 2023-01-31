94668820e2f31a5b94ef573e95fc67f5d94354c7
MichelYAM <michel.yam92@gmail.com>
2023-01-31 21:03:03 +0100

    test

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import { MyCounter } from 'my-react-typescript-package'
-
+import { Datapicker } from "my-react-datapickerzadzadaz"
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const [test, setTest] = useState("")
 root.render(
   <React.StrictMode>
     <div>
@@ -13,6 +14,7 @@ root.render(
     <div>
       <h2>Counter with predefined value</h2>
       <MyCounter value={5} />
+      <Datapicker dataFormat='DD/MM/YYYY' selectedDate={test} setSelectedDate={setTest} />
     </div>
   </React.StrictMode>,
 )
