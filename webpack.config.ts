ec25e8dd4bd3d9d0f1275e8e1ea0208406d4d156
MichelYAM <michel.yam92@gmail.com>
2023-02-01 13:59:37 +0100

    test

--- webpack.config.ts
+++ webpack.config.ts
@@ -6,7 +6,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.ts'),
   output: {
-    filename: 'bundle.js',
+    filename: 'Datapicker.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
