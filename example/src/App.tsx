d6a223898c3e6906ad1b8f416de3d45fe30bdfa1
MichelYAM <michel.yam92@gmail.com>
2023-02-01 22:19:43 +0100

    test

---
+++ example/src/App.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import { Datapicker } from 'my-react-datapickerzadzadaz'
+import "my-react-datapickerzadzadaz/dist/stylesheets/datapicker.css"
+export default function App() {
+    const [startDate, setStartDate] = useState("")
+
+    const getYear = (date: Date) => {
+        return date.getFullYear()
+    }
+
+    const range = (start: number, end: number) => {
+        const year = [];
+        for (let i = start; i <= end; i++) {
+            year.push(i);
+        }
+        return year;
+    }
+    const years = range(1990, getYear(new Date()) + 10);
+    const months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
+    const mystyle = {
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    };
+    return (
+        <div style={mystyle}>
+            <Datapicker
+                customHeader={({
+                    currentMonth,
+                    currentYear,
+                    changeMonth,
+                    changeYear,
+                    prev,
+                    next,
+                }: any) => (
+                    <div>
+                        <button onClick={prev}>
+                            {"<"}
+                        </button>
+                        <select
+                            value={months[currentMonth as unknown as number]}
+                            onChange={({ target: { value } }) =>
+                                changeMonth(months.indexOf(value))
+                            }
+                        >
+                            {months.map((option) => (
+                                <option key={option} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+                        <select
+                            value={currentYear}
+                            onChange={({ target: { value } }: any) => changeYear(value)}
+                        >
+                            {years.map((option) => (
+                                <option key={option} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+                        <button onClick={next} >
+                            {">"}
+                        </button>
+                    </div>
+                )}
+                dataFormat='DD/MM/YYYY' selectedDate={startDate} setSelectedDate={setStartDate} />
+        </div>
+    )
+}
--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { MyCounter } from 'my-react-typescript-package'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import { MyCounter } from 'my-react-typescript-package'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+// root.render(
+//   <React.StrictMode>
+//     <div>
+//       <h2>Default counter</h2>
+//       <MyCounter />
+//     </div>
+//     <hr />
+//     <div>
+//       <h2>Counter with predefined value</h2>
+//       <MyCounter value={5} />
+//     </div>
+//   </React.StrictMode>,
+// )
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
   <React.StrictMode>
-    <div>
-      <h2>Default counter</h2>
-      <MyCounter />
-    </div>
-    <hr />
-    <div>
-      <h2>Counter with predefined value</h2>
-      <MyCounter value={5} />
-    </div>
+    <App />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
